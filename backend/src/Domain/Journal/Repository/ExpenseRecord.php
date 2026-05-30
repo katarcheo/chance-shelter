77d273a67498ffa6937e5a25745cb8e180f612b0
Oleg <me@katarcheo.dev>
2026-05-30 16:48:46 +0300

    record dtos

--- backend/src/Domain/Journal/Repository/ExpenseRecord.php
+++ backend/src/Domain/Journal/Repository/ExpenseRecord.php
@@ -2,7 +2,15 @@
 
 namespace App\Domain\Journal\Repository;
 
+use App\Domain\Money;
+
 final readonly class ExpenseRecord
 {
-
+    public function __construct(
+        public Money $amount,
+        public string $categoryName,
+        public string $categoryId,
+        public string $description,
+    )
+    {}
 }
--- backend/src/Domain/Journal/Repository/IncomeRecord.php
+++ backend/src/Domain/Journal/Repository/IncomeRecord.php
@@ -2,6 +2,14 @@
 
 namespace App\Domain\Journal\Repository;
 
+use App\Domain\Money;
+
 final readonly class IncomeRecord
 {
+    public function __construct(
+        public Money  $amount,
+        public string $fundName,
+        public string $fundId,
+    )
+    {}
 }
