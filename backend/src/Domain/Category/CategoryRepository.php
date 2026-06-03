8069ce5e2441dd5534bd2dee283065db4baaa847
Oleg <me@katarcheo.dev>
2026-06-03 22:38:33 +0300

    contract fixes

--- backend/src/Domain/Category/CategoryRepository.php
+++ backend/src/Domain/Category/CategoryRepository.php
@@ -4,6 +4,5 @@ namespace App\Domain\Category;
 
 interface CategoryRepository
 {
-    public function find(string $id): ?Category;
     public function isExistByName(string $name): bool;
 }
--- backend/src/Infrastructure/Doctrine/DoctrineCategoryRepository.php
+++ backend/src/Infrastructure/Doctrine/DoctrineCategoryRepository.php
@@ -2,18 +2,13 @@
 
 namespace App\Infrastructure\Doctrine;
 
-use App\Domain\Category\Category;
 use App\Domain\Category\CategoryRepository;
+use Doctrine\ORM\EntityRepository;
 
-class DoctrineCategoryRepository extends DoctrineBaseRepository implements CategoryRepository
+class DoctrineCategoryRepository extends EntityRepository implements CategoryRepository
 {
     public function isExistByName(string $name): bool
     {
         // TODO: Implement isExistByName() method.
-    }
-
-    public function save(Category $category): void
-    {
-        $this->simpleSave($category);
     }
 }
