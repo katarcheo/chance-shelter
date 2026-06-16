f836c4140db6768503d7026996878d35683defa7
Oleg <me@katarcheo.dev>
2026-06-16 18:19:11 +0300

    repository has got from container

--- backend/tests/Infrastructure/Doctrine/DoctrineCategoryRepositoryTest.php
+++ backend/tests/Infrastructure/Doctrine/DoctrineCategoryRepositoryTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Domain\Category\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 pest()->group('infrastructure');
@@ -7,5 +8,7 @@ pest()->group('infrastructure');
 uses(KernelTestCase::class);
 
 test('dev', function () {
-
+    $this::bootKernel();
+    $repo = $this::getContainer()->get(CategoryRepository::class);
+    dump($repo);
 });
