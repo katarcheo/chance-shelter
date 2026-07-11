2f79cb65aa38eadd2692d8c65ea81ecb3ec2f48e
Oleg <me@katarcheo.dev>
2026-07-12 00:48:19 +0300

    dev expense form

--- backend/src/Application/UseCases/JournalRecording/RecordExpense/RecordExpenseService.php
+++ backend/src/Application/UseCases/JournalRecording/RecordExpense/RecordExpenseService.php
@@ -7,7 +7,9 @@ use App\Domain\Category\CategoryRepository;
 use App\Domain\Journal\Expense\ExpenseMedia;
 use App\Domain\Journal\Repository\JournalRepository;
 use App\Domain\Money;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
+#[AsMessageHandler(bus: 'messenger.bus.command')]
 class RecordExpenseService
 {
     public function __construct(
@@ -19,25 +21,25 @@ class RecordExpenseService
 
     public function __invoke(CreateExpenseCommand $expenseData): CreatedExpenseResult
     {
-        if (!$category = $this->categoryRepo->find($expenseData->categoryId)) {
-            throw new ApplicationException("Category not found");
-        }
-
-        $balance = $this->journalRepo->lockCurrentBalance();
-
-        $expense = $balance->applyExpense(
-            amount: Money::fromFloat($expenseData->amount),
-            category: $category,
-            description: $expenseData->description,
-        );
-
-        foreach ($expenseData->media as $key => $file) {
-            $file->move($this->mediaDir, "{$expense->id()}_$key.{$file->guessExtension()}");
-            $expense->attachMedia(new ExpenseMedia($file->path));
-        }
-
-        $this->journalRepo->save($balance);
-
+        dd($expenseData);
+//        if (!$category = $this->categoryRepo->find($expenseData->categoryId)) {
+//            throw new ApplicationException("Category not found");
+//        }
+//
+//        $balance = $this->journalRepo->lockCurrentBalance();
+//
+//        $expense = $balance->applyExpense(
+//            amount: Money::fromFloat($expenseData->amount),
+//            category: $category,
+//            description: $expenseData->description,
+//        );
+//
+//        foreach ($expenseData->media as $key => $file) {
+//            $file->move($this->mediaDir, "{$expense->id()}_$key.{$file->guessExtension()}");
+//            $expense->attachMedia(new ExpenseMedia($file->path));
+//        }
+//
+//        $this->journalRepo->save($balance);
         return CreatedExpenseResult::fromExpense($expense);
     }
 }
